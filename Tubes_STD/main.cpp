16eda41764370975c77f0f98ee28b27a7040f4dd
Nama Anda <email@domain.com>
2024-12-12 10:48:26 +0700

    Initial Commit

---
+++ Tubes_STD/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tubes_STD
+//
+//  Created by Muhammad Arzu on 12/12/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
